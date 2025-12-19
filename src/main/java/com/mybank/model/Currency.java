e3666b0e2130d63b1da1e19f283fbc37e6bc7bc5
JayXie9499 <tsum950210@gmail.com>
2025-12-19 20:48:44 +0800

    feat(currency): add id attribute to class
    - Add `id` field to `Currency.java`

--- src/main/java/com/mybank/model/Currency.java
+++ src/main/java/com/mybank/model/Currency.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Currency {
+    public final int id;
     public final String name;
     public final double exchangeRate;
 
@@ -14,11 +15,12 @@ public class Currency {
         final List<Currency> currencies = new ArrayList<>();
         final Connection conn = Database.getConnection();
         final Statement stmt = conn.createStatement();
-        final ResultSet rs = stmt.executeQuery("SELECT * FROM banks;");
+        final ResultSet rs = stmt.executeQuery("SELECT * FROM currencies;");
         while (rs.next()) {
+            final int id = rs.getInt("id");
             final String name = rs.getString("name");
             final double exchangeRate = rs.getDouble("exchangeRate");
-            final Currency currency = new Currency(name, exchangeRate);
+            final Currency currency = new Currency(id, name, exchangeRate);
             currencies.add(currency);
         }
         rs.close();
@@ -27,7 +29,8 @@ public class Currency {
         return currencies;
     }
 
-    private Currency(String name, double exchangeRate) {
+    private Currency(int id, String name, double exchangeRate) {
+        this.id = id;
         this.name = name;
         this.exchangeRate = exchangeRate;
     }
